2ce9b25106e1a99c3c68500b5838c59d6c3b6177
Ivan Pedrazas <ipedrazas@gmail.com>
2014-04-08 16:50:07 +0100

    ohmyz.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,8 @@
-
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-sudo apt-get install git vim zsh terminator curl virtualbox vagrant
+sudo apt-get install git vim zsh terminator curl virtualbox vagrant conky
+
+
+curl -L http://install.ohmyz.sh | sh
+
+chsh -s /bin/zsh
