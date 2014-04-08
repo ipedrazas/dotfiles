43e66547f3df2c80f31800cd17eae270b7416ce2
Ivan Pedrazas <ipedrazas@gmail.com>
2014-04-08 16:53:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ ln -sh dotfiles/bin bin
 ln -sh dotfiles/gitconfig .gitconfig
 ln -sh dotfiles/vim .vimrc
 ln -sh dotfiles/zshrc .zshrc
+
+
+git config --global user.name "Ivan Pedrazas"
+git config --global user.email ipedrazas@gmail.com
+git config --global core.editor vim
