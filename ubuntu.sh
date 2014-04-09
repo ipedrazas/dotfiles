14ab02111147e82b8f795ffe1b2c802c0c83f782
Ivan Pedrazas <ipedrazas@gmail.com>
2014-04-09 11:27:35 +0100

    ubuntu install files

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,14 +1,13 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-sudo apt-get install git vim zsh terminator curl virtualbox vagrant conky
+sudo apt-get install git vim zsh terminator curl virtualbox vagrant conky vlc
 
 
 curl -L http://install.ohmyz.sh | sh
 chsh -s /bin/zsh
 
-# Dropbox
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-~/.dropbox-dist/dropboxd &
 
+sh ubuntu/sublime-install.sh
+sh ubuntu/dropbox-install.sh
 
 
---
+++ ubuntu/sublime-install.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
+sudo apt-get update -y
+sudo apt-get install sublime-text-installer -y
