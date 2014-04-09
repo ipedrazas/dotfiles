cfc19756046613ea0e2e9affce6fb5c1df4bcf78
Ivan Pedrazas <ipedrazas@gmail.com>
2014-04-09 11:30:21 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,8 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-sudo apt-get install git vim zsh terminator curl virtualbox vagrant conky vlc
+sudo apt-get install ubuntu-restricted-extras
+
+sudo apt-get install git vim zsh terminator curl virtualbox vagrant conky vlc flashplugin-installer unity-tweak-tool dropbox
 
 
 curl -L http://install.ohmyz.sh | sh
@@ -8,6 +10,8 @@ chsh -s /bin/zsh
 
 
 sh ubuntu/sublime-install.sh
-sh ubuntu/dropbox-install.sh
+# sh ubuntu/dropbox-install.sh
+
+gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
 
