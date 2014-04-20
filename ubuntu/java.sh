61fe0a3b3169ec861ec2d29a8ab088aa991c5502
Ivan Pedrazas <ipedrazas@gmail.com>
2014-04-20 20:42:53 +0100

    Update java.sh

--- ubuntu/java.sh
+++ ubuntu/java.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository ppa:webupd8team/java -y
 
 sudo apt-get update -y
 
-sudo apt-get install oracle-java7-installer oracle-java8-installer install oracle-java7-set-default install oracle-java8-set-default
+sudo apt-get install oracle-java7-installer oracle-java8-installer oracle-java7-set-default oracle-java8-set-default
 
 # set java 8 as default
 sudo update-java-alternatives -s java-8-oracle
