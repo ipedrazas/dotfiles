cc988731da789bbe4d10367b9baf774692f31fcf
Ivan Pedrazas <ipedrazas@gmail.com>
2014-04-21 23:10:12 +0100

    Create eclipse.sh

---
+++ ubuntu/eclipse.sh
@@ -0,0 +1,8 @@
+#
+
+
+wget https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz&mirror_id=529
+
+tar -zxvf eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz && sudo mv eclipse /opt
+
+ln -s /opt/eclipse/eclipse /usr/local/bin/eclipse
