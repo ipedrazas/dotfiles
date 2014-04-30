78c7f5affbd4362aa2f2d6d9ce5aebe0693b270f
Ivan Pedrazas <ipedrazas@gmail.com>
2014-04-30 23:27:16 +0100

    apple keyboard added

---
+++ keyboard.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+address="44:2A:60:E2:AC:44"
+
+while (sleep 1)
+do
+	connected='sudo hidd --show' # > /dev/null
+	if [[ ! $connected =~ .*${address}.* ]] ; then
+		sudo hidd --connect ${address} # > /dev/null 2>&1
+	fi
+done
+
