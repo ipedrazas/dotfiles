d3e1ab754196ed51d0d8ea3109ff34a4d88776c6
Ivan Pedrazas <ipedrazas@gmail.com>
2014-11-26 14:21:15 +0000

    Update sublime-install.sh

--- ubuntu/sublime-install.sh
+++ ubuntu/sublime-install.sh
@@ -1,3 +1,8 @@
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
 sudo apt-get update -y
 sudo apt-get install sublime-text-installer -y
+
+
+cd ~/.config/sublime-text-3/Packages
+rm -Rf User
+git clone git@github.com:ipedrazas/sublimetext-settings.git User
