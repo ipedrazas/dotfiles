2948035009a97073a4b5a05577d11520896789fc
Ivan Pedrazas <ipedrazas@gmail.com>
2015-10-21 11:55:35 +0100

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -10,6 +10,7 @@ wget https://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.4.0.zip
 unzip font-awesome-4.4.0.zip
 cp font-awesome-4.4.0/fonts/fontawesome-webfont.ttf ~/.i3
 rm -rf font-awesome-4.4.0
+rm font-awesome-4.4.0.zip
 
 wget http://www.fontsquirrel.com/fonts/download/M-1m
 unzip M-1m
@@ -21,3 +22,4 @@ wget https://github.com/driftyco/ionicons/archive/v2.0.1.zip
 unzip v2.0.1.zip
 cp ionicons-2.0.1/fonts/ionicons.ttf ~/.i3
 rm -rf ionicons-2.0.1
+rm v2.0.1.zip
