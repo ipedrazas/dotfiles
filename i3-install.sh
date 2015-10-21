ccb74c9e2e08f2797d42bb88488b7e25cefdcd3c
Ivan Pedrazas <ipedrazas@gmail.com>
2015-10-21 11:40:33 +0100

    Create i3-install.sh

---
+++ i3-install.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+sudo apt-get install -y i3 dunst i3lock
+wget http://cz.archive.ubuntu.com/ubuntu/pool/universe/i/i3status/i3status_2.9-2_amd64.deb
+sudo dpkg -i i3status_2.9-2_amd64.deb
+rm dpkg -i i3status_2.9-2_amd64.deb
