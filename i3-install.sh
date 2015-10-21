49416f066f3a0457af6a0cf1c0b5555ff05226ea
Ivan Pedrazas <ipedrazas@gmail.com>
2015-10-21 12:02:49 +0100

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -9,17 +9,27 @@ rm i3status_2.9-2_amd64.deb
 wget https://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.4.0.zip
 unzip font-awesome-4.4.0.zip
 cp font-awesome-4.4.0/fonts/fontawesome-webfont.ttf ~/.i3
+mkdir -p ~/.fonts/truetype/font-awesome
+cp font-awesome-4.4.0/fonts/fontawesome-webfont.ttf ~/.fonts/truetype/font-awesome
 rm -rf font-awesome-4.4.0
 rm font-awesome-4.4.0.zip
 
 wget http://www.fontsquirrel.com/fonts/download/M-1m
 unzip M-1m
 cp mplus-*.ttf ~/.i3
+mkdir -p ~/.fonts/truetype/M-1m
+cp mplus-*.ttf ~/.fonts/truetype/M-1m
 rm M-1m
 rm *.ttf
+rm M+*.*
 
 wget https://github.com/driftyco/ionicons/archive/v2.0.1.zip
 unzip v2.0.1.zip
 cp ionicons-2.0.1/fonts/ionicons.ttf ~/.i3
+mkdir -p ~/.fonts/truetype/font-awesome-4.4.0
+cp ionicons-2.0.1/fonts/ionicons.ttf ~/.fonts/truetype/ionicons
 rm -rf ionicons-2.0.1
 rm v2.0.1.zip
+
+# refresh fonts
+sudo fc-cache -f -v
