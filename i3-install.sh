ae28d08c003f8dc3f4aad72aee06c27e6ac210b5
Ivan Pedrazas <ipedrazas@gmail.com>
2015-10-21 11:42:38 +0100

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -3,4 +3,4 @@
 sudo apt-get install -y i3 dunst i3lock
 wget http://cz.archive.ubuntu.com/ubuntu/pool/universe/i/i3status/i3status_2.9-2_amd64.deb
 sudo dpkg -i i3status_2.9-2_amd64.deb
-rm dpkg -i i3status_2.9-2_amd64.deb
+rm i3status_2.9-2_amd64.deb
