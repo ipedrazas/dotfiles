3ef3626f1a715d66ff393282300df90868307b9d
Ivan Pedrazas <ipedrazas@gmail.com>
2015-10-21 14:36:48 +0100

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-sudo apt-get install -y i3 dunst i3lock zip
+sudo apt-get install -y i3 dunst i3lock zip lxappearance gtk-chtheme qt4-qtconfig
 wget http://cz.archive.ubuntu.com/ubuntu/pool/universe/i/i3status/i3status_2.9-2_amd64.deb
 sudo dpkg -i i3status_2.9-2_amd64.deb
 rm i3status_2.9-2_amd64.deb
