238f0457fd57818b20309030faf3e84374e41d79
Ivan Pedrazas <ipedrazas@gmail.com>
2015-10-21 11:49:18 +0100

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -1,6 +1,16 @@
 #! /usr/bin/env bash
 
-sudo apt-get install -y i3 dunst i3lock
+sudo apt-get install -y i3 dunst i3lock zip
 wget http://cz.archive.ubuntu.com/ubuntu/pool/universe/i/i3status/i3status_2.9-2_amd64.deb
 sudo dpkg -i i3status_2.9-2_amd64.deb
 rm i3status_2.9-2_amd64.deb
+
+# download fonts
+wget https://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.4.0.zip
+unzip font-awesome-4.4.0.zip
+cp font-awesome-4.4.0/fonts/fontawesome-webfont.ttf ~/.i3
+rm -rf font-awesome-4.4.0
+
+wget http://www.fontsquirrel.com/fonts/download/M-1m
+unzip M-1m.zip
+cp mplus-*.ttf ~/.i3
