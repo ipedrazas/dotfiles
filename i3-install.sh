5ccdc79ee99e268069279bb5f3db7dcde8b91ad7
Ivan Pedrazas <ipedrazas@gmail.com>
2016-05-11 14:42:25 +0100

    improved i3 exp

--- i3-install.sh
+++ i3-install.sh
@@ -6,7 +6,7 @@ cp ./i3status.conf ~/.i3/
 
 sudo apt-add-repository -y ppa:yktooo/ppa
 
-sudo apt-get install -y i3 dunst i3lock zip lxappearance gtk-chtheme qt4-qtconfig indicator-sound-switcher gnome-icon-theme-full
+sudo apt-get install -y i3 dunst i3lock zip lxappearance gtk-chtheme qt4-qtconfig indicator-sound-switcher gnome-icon-theme-full unclutter
 wget http://cz.archive.ubuntu.com/ubuntu/pool/universe/i/i3status/i3status_2.9-2_amd64.deb
 sudo dpkg -i i3status_2.9-2_amd64.deb
 rm i3status_2.9-2_amd64.deb
