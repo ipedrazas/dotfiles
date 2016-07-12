4b1e3f1318ec2d3cdd22fc341420e0ca5bc0ce67
Ivan Pedrazas <ipedrazas@gmail.com>
2016-07-12 10:36:27 +0100

    Update vagrant-ubuntu.sh

--- ubuntu/vagrant-ubuntu.sh
+++ ubuntu/vagrant-ubuntu.sh
@@ -11,9 +11,3 @@ sudo apt-get install git vim zsh curl python-pip python-dev -y
 curl -L http://install.ohmyz.sh | sh
 chsh -s /bin/zsh
 
-
-sudo pip install virtualenvwrapper
-export WORKON_HOME=~/Envs
-mkdir -p $WORKON_HOME
-source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv ubuntu
