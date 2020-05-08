d387e4d5f97a081f21b4eff0636a43715606011b
Ivan Pedrazas <ipedrazas@gmail.com>
2020-05-08 15:28:38 +0100

    chore: refactor baraction

--- baraction.sh
+++ baraction.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
-# baraction.sh for spectrwm status bar
+# minimal statusbar info : Spandan # github.com/spandanji
 
-## DISK
-hdd() {
-  hdd="$(df -h | awk 'NR==4{print $3, $5}')"
-  echo -e "HDD: $hdd"
+public_ip(){
+    pub_ip=$(curl 'https://api.ipify.org?format=txt')
+    echo -e "Public IP: $pub_ip"
 }
 
-## RAM
-mem() {
-  mem=`free | awk '/Mem/ {printf "%dM/%dM\n", $3 / 1024.0, $2 / 1024.0 }'`
-  echo -e "$mem"
+hdd(){
+	hdd="$(df -h | grep "/dev/mapper/vgxubuntu-root" | awk '{print $3, $5}')"
+	echo -e "HD : $hdd"
 }
 
-## CPU
+hdd_data(){
+    data="$(df -h | grep sda2 | awk '{print $3, $5}')"
+    echo -e "DATA : $data" 
+}
+
+mem(){
+	mem="$(free | awk '/Mem/ {printf "%d MiB / %d MiB : %d%%\n", $3 / 1024.0, $2 / 1024.0,  $3/$2 *100}')"
+	echo -e "MEM : $mem"
+}
+
+
 cpu() {
-  read cpu a b c previdle rest < /proc/stat
-  prevtotal=$((a+b+c+previdle))
-  sleep 0.5
-  read cpu a b c idle rest < /proc/stat
-  total=$((a+b+c+idle))
-  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-  echo -e "CPU: $cpu%"
+	read cpu a b c previdle rest < /proc/stat
+  	prevtotal=$((a+b+c+previdle))
+  	sleep 0.5
+  	read cpu a b c idle rest < /proc/stat
+
+	total=$((a+b+c+idle))
+ 	cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+	echo -e "CPU: $cpu%"
 }
 
-## VOLUME
-vol() {
-    vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g'`
-    echo -e "VOL: $vol"
+vol(){
+	x="$(amixer -D pulse get Master | awk -F 'Left:|[][]' 'BEGIN {RS=""}{ print $5 }')"
+	if [ $x = "off" ]
+	then
+		vol="MUTE"
+	else
+		vol="$(amixer -D pulse get Master | awk -F 'Left:|[][]' 'BEGIN {RS=""}{ print $3 }')"
+	fi
+	echo -e "VOL: $vol"
 }
 
-SLEEP_SEC=3
-#loops forever outputting a line every SLEEP_SEC secs
 
-# It seems that we are limited to how many characters can be displayed via
-# the baraction script output. And the the markup tags count in that limit.
-# So I would love to add more functions to this script but it makes the 
-# echo output too long to display correctly.
+SLEEP_SEC=5
 while :; do
-    echo "+@fg=1; +@fn=1;💻+@fn=0; $(cpu) +@fg=0; | +@fg=2; +@fn=1;💾+@fn=0; $(mem) +@fg=0; | +@fg=3; +@fn=1;💿+@fn=0; $(hdd) +@fg=0; | +@fg=4; +@fn=1;🔈+@fn=0; $(vol) +@fg=0; |"
+    echo "$(cpu) || $(hdd) || $(hdd_data) || $(mem) ||  $(vol) || $(public_ip)" 
 	sleep $SLEEP_SEC
 done
-
