9cc14824012fe25acf1c3371a2d1d11f34730891
Ivan Pedrazas <ipedrazas@gmail.com>
2022-03-19 17:15:00 +0000

    feat: add osx init script

---
+++ init-osx.sh
@@ -0,0 +1,58 @@
+
+#!/bin/sh
+
+# Homebrew Script for OSX
+# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
+
+echo "Installing brew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap homebrew/cask
+
+
+brew install 1password 1password-cli
+brew install age
+brew install argo
+brew install asdf
+brew install awscli
+brew install brave-browser
+brew install cosign
+brew install cue
+brew install dagger
+brew install dash
+brew install discord
+brew install docker
+brew install firefox
+brew install flux
+brew install git
+brew install gnupg
+brew install go
+brew install google-chrome
+brew install helm
+brew install htop
+brew install httpie
+brew install hugo
+brew install iterm2
+brew install jq
+brew install ko
+brew install kubernetes-cli
+brew install kustomize
+brew install mas # cli mac app store
+brew install mosh
+brew install nextcloud
+brew install nmap
+brew install node
+brew install nvm
+brew install obsidian
+brew install pinentry-mac
+brew install pulumi
+brew install qt
+brew install slack
+brew install sops
+brew install sqlite
+brew install ssh-copy-id
+brew install tailscale
+brew install tree
+brew install wally
+brew install visual-studio-code
+brew install yq
+brew install zoom
